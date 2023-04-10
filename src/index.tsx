73fe80b52270e15751a67454a7c26d23cf4b825a
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-10 17:59:25 +0900

    refs #2 fix use InputAffiliateCode, ShowPHPCode

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
-import { render } from '@wordpress/element';
+import { render, useState } from '@wordpress/element';
+
+import { InputAffiliateCode } from 'src/components/InputAffiliateCode';
+import { ShowPHPCode } from 'src/components/ShowPHPCode';
 
 const CodeSnippetsGenerator = () => {
+	const [ affiliateCode, setAffiliateCode ] = useState( '' );
 	return (
 		<div>
 			<h2>Code Snippetsリンク生成ツール</h2>
 			<p>WordPressプラグインのCode Snippetsを使って主にアフィリエイトコード等のリンクを生成するツールです。</p>
 			<p>以下の入力フォームにアフィリエイトコードをコピー&ペーストすると<code>function</code>から始まるPHPコードが出力されます。</p>
 			<p>Code Snippetsに出力されたコードをペーストし保存するとWordPressで使用できるショートコードが使えるようになります。</p>
+			<InputAffiliateCode
+				setAffiliateCode={ setAffiliateCode }
+			/>
+			<ShowPHPCode affiliateCode={ affiliateCode } />
 		</div>
 	);
 };

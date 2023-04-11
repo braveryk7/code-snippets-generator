f9918faeac5b6a07a873b127fc79fd1e16e88359
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-12 01:25:02 +0900

    refs #4 add ShowUsage

--- src/components/ShowUsage.tsx
+++ src/components/ShowUsage.tsx
@@ -1,5 +1,10 @@
-export const ShowUsage = () => {
+export const ShowUsage = ( props: { characterString: string} ) => {
+	const { characterString } = props;
+
 	return (
-		<></>
+		<>
+			<h3>使用イメージ</h3>
+			<p>現在[{ characterString }]は期間限定で30％オフです！</p>
+		</>
 	);
 };

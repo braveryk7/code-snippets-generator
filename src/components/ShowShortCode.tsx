51399c7cbf940cfbe6d0321ddcb79b53fccf2f08
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-12 01:42:39 +0900

    refs #6 add label, input form

--- src/components/ShowShortCode.tsx
+++ src/components/ShowShortCode.tsx
@@ -1,6 +1,21 @@
-export const ShowShortCode = () => {
+export const ShowShortCode = ( props: { characterString: string} ) => {
+	const { characterString } = props;
+
 	return (
 		<>
+			<label
+				className="cxn-label"
+				htmlFor="show-shortcode"
+			>
+				ショートコード
+			</label>
+			<input
+				type="text"
+				className="cxn-show-shortcode"
+				id="show-shortcode"
+				value={ `[${ characterString }]` }
+				readOnly
+			/>
 		</>
 	);
 };

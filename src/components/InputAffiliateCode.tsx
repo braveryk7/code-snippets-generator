b31af2121efc384b59578197123ba8547d01ba79
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-11 17:26:54 +0900

    refs #2 fix use useEffect, add placeholder

--- src/components/InputAffiliateCode.tsx
+++ src/components/InputAffiliateCode.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useEffect } from 'react';
 
 import { TextControl } from '@wordpress/components';
 import { useState } from '@wordpress/element';
@@ -9,11 +9,14 @@ export const InputAffiliateCode = (
 	const [ formValue, setFormValue ] = useState( '' );
 	const { setAffiliateCode } = props;
 
-	setAffiliateCode( formValue );
+	useEffect( () => {
+		setAffiliateCode( formValue );
+	}, [ formValue, setAffiliateCode ] );
 	return (
 		<>
 			<TextControl
 				label="アフィリエイトコード"
+				placeholder='例: <a href="https://example.com">example</a>'
 				value={ formValue }
 				onChange={ ( value ) => setFormValue( value ) }
 			/>

4f5168ffbbb4e832e7ff227fdda4ac50e45ce0b2
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-13 14:24:57 +0900

    refs #11 add wrap use TimelineItem component

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { InputAffiliateCode } from 'src/components/InputAffiliateCode';
 import { ShowPHPCode } from 'src/components/ShowPHPCode';
 import { ShowShortCode } from 'src/components/ShowShortCode';
 import { ShowUsage } from 'src/components/ShowUsage';
+import { TimelineItem } from 'src/components/layout/TimelintItem';
 
 const CodeSnippetsGenerator = () => {
 	const [ affiliateCode, setAffiliateCode ] = useState( '' );
@@ -17,17 +18,23 @@ const CodeSnippetsGenerator = () => {
 			<p>WordPressプラグインのCode Snippetsを使って主にアフィリエイトコード等のリンクを生成するツールです。</p>
 			<p>以下の入力フォームにアフィリエイトコードをコピー&ペーストすると<code>function</code>から始まるPHPコードが出力されます。</p>
 			<p>Code Snippetsに出力されたコードをペーストし保存するとWordPressで使用できるショートコードが使えるようになります。</p>
-			<InputAffiliateCode
-				setAffiliateCode={ setAffiliateCode }
-			/>
-			<ShowPHPCode
-				affiliateCode={ affiliateCode }
-				PHPCode={ PHPCode }
-				setPHPCode={ setPHPCode }
-				characterString={ characterString }
-				setCharacterString={ setCharacterString }
-			/>
-			<ShowShortCode characterString={ characterString } />
+			<TimelineItem itemNumber={ 1 } itemHeading="アフィリエイトコードを入力">
+				<InputAffiliateCode
+					setAffiliateCode={ setAffiliateCode }
+				/>
+			</TimelineItem>
+			<TimelineItem itemNumber={ 2 } itemHeading="PHPコード">
+				<ShowPHPCode
+					affiliateCode={ affiliateCode }
+					PHPCode={ PHPCode }
+					setPHPCode={ setPHPCode }
+					characterString={ characterString }
+					setCharacterString={ setCharacterString }
+				/>
+			</TimelineItem>
+			<TimelineItem itemNumber={ 3 } itemHeading="ショートコード">
+				<ShowShortCode characterString={ characterString } />
+			</TimelineItem>
 			<ShowUsage characterString={ characterString } />
 		</div>
 	);

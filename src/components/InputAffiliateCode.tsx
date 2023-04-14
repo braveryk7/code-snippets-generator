e45c422d3d45e8acfc90eb6d6685503094847acc
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-14 17:24:48 +0900

    refs #13 fix shortcode form help text

--- src/components/InputAffiliateCode.tsx
+++ src/components/InputAffiliateCode.tsx
@@ -50,7 +50,7 @@ export const InputAffiliateCode = (
 						className="cxn-advanced-settings-item-shortcode"
 						label="ショートコードを任意の文字列に変更する"
 						placeholder="例: アフィリエイトリンク1"
-						help="分かりやすい値を設定しておくと便利です。日本語、数字、アルファベット、記号全て使用可能です。"
+						help="分かりやすい値を設定しておくと便利です。空欄の場合、自動生成されます。"
 						value={ shortcode }
 						onChange={ ( value ) => setShortcode( value ) }
 					/>

0b1b22332fda1ebc5b302114c8a736dd9e852794
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-16 15:22:21 +0900

    refs #8 add copied class, add, remove

--- src/utils/copyToClipboard.ts
+++ src/utils/copyToClipboard.ts
@@ -1,9 +1,34 @@
 export const copyToClipboard = (
 	selector: keyof HTMLElementTagNameMap,
+	copyFormName: 'cxn-show-php' | 'cxn-show-shortcode',
 	e?: React.MouseEvent<HTMLElement, MouseEvent>,
 ) => {
 	const target = e
 		? e.target as HTMLTextAreaElement
 		: document.querySelector( selector ) as HTMLTextAreaElement;
 	navigator.clipboard.writeText( target.value );
+
+	if ( copyFormName ) {
+		const formId = document.getElementById( copyFormName );
+		if ( formId ) {
+			formId.classList.add( 'cxn-copied' );
+		}
+	}
+
+	const removeCxnCopiedClass = (): void => {
+		const elements = document.querySelectorAll<HTMLElement>( '.cxn-copied' );
+
+		if ( elements.length > 0 ) {
+			elements.forEach( ( element: HTMLElement & { timeout?: number } ) => {
+				if ( element.timeout ) {
+					clearTimeout( element.timeout );
+				}
+				window.setTimeout( () => {
+					element.classList.remove( 'cxn-copied' );
+				}, 2000 );
+			} );
+		}
+	};
+
+	removeCxnCopiedClass();
 };
